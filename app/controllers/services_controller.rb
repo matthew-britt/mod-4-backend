81496a6a08aad13b42dcca905c2fb4d59c615f90
github username <github email address>
2020-04-14 16:53:05 -0500

    data seeded

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,18 @@
 class ServicesController < ApplicationController
+    # def index
+    #     services = Service.search(search_params.to_h.symbolize_keys)
+    #     render json: services
+    # end
+
     def index
-        services = Service.search(search_params.to_h.symbolize_keys)
-        render json: services
-      end
+        services = Service.all 
+        render json: services     
+    end
+
     
       private
     
-      def search_params
-        params.permit(:min_lng, :max_lng, :min_lat, :max_lat)
-      end
+    #   def search_params
+    #     params.permit(:min_lng, :max_lng, :min_lat, :max_lat)
+    #   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'database_cleaner'
 
-a = User.create(name: 'matt')
-b = Service.create(name: 'haircut')
-c = ServiceUser.create(user_id: 1, service_id: 1)
+DatabaseCleaner.clean_with(:truncation)
+# User.delete_all
+# Service.delete_all
+# ServiceUser.delete_all
 
 center_point = { lat: 30.2672, lng: -97.7431 }
+
+service_types = ["lawn service", "haircut", "toilet paper", "dog walking", "food delivery"]
+
+25.times do 
+    User.create(name: Faker::Name.name)
+end
+
+50.times do 
+    Service.create(
+    name: service_types.sample, 
+    price: rand(3..30),
+    longitude: center_point[:lng] + rand(-0.10..0.10),
+    latitude: center_point[:lat] + rand(-0.10..0.10)
+    )
+end
+
+25.times do
+    su = ServiceUser.new
+    su.user = User.all.sample
+    su.service = Service.all.sample
+    su.save
+end
+
