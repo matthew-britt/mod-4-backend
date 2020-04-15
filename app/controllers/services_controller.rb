a98b8f998dc8a8aa19727fee0fdde34c886d06b7
github username <github email address>
2020-04-15 17:39:13 -0500

    service and user controllers added

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,8 +9,39 @@ class ServicesController < ApplicationController
         render json: services     
     end
 
+    def show
+        service = Service.find(params[:id])
+        render json: service 
+    end
+
+    def create
+        service = Service.create(service_params)
+        render json: service 
+    end
+
+    def update
+        @service = Service.find(params[:id])
+        @service.update(service_params)
+        render json: @service
+    end
+
+    def destroy
+        @service = Service.find(params[:id])
+        @service.destroy    
+        render json: {message: "Service no longer available."}
+    end
+
     
-      private
+    private
+
+    def service_params
+        params.permit(:name, :price, :longitude, :latitude, :provided_by, :phone, :email)
+    end
+
+    # def set_service
+    #     @service = Service.find(params[:id])
+    # end
+
     
     #   def search_params
     #     params.permit(:min_lng, :max_lng, :min_lat, :max_lat)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,32 @@ class UsersController < ApplicationController
         render json: users     
     end
 
+    def show
+        user = User.find(params[:id])
+        render json: user 
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user 
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render json: @user
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {message: "User has perished."}
+    end
+
+    
+    private
+
+    def user_params
+        params.permit(:name)
+    end
 end
